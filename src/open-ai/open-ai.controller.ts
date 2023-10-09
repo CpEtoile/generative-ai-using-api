122928f82999e9f90e25bbf5163ddc3aa5a73b00
Chunpeng XING <chunpeng@hotmail.fr>
2023-10-10 01:24:56 +0200

    feat: route for few shot prompting

--- src/open-ai/open-ai.controller.ts
+++ src/open-ai/open-ai.controller.ts
@@ -1,19 +1,34 @@
-import { Controller, Get, Param } from "@nestjs/common";
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { OpenAiService } from './open-ai.service';
 
+export class FewShotPrompt {
+  prompts: RolePrompt[];
+}
+
 @ApiTags('open-ai')
-@Controller('single-form')
+@Controller('open-ai')
 export class OpenAiController {
   constructor(private readonly openAiService: OpenAiService) {}
-
-  @Get(':noun')
-  async callOpenAiApi(@Param('noun') noun: string): Promise<string> {
-    return await this.openAiService.getSingleFormOfANoun(noun);
-  }
 
   @Get('test')
   async testOpenAiApi(): Promise<string> {
     return await this.openAiService.callOpenAiTest();
   }
+
+  @Get('single-form:noun')
+  async callOpenAiApi(@Param('noun') noun: string): Promise<string> {
+    return await this.openAiService.getSingleFormOfANoun(noun);
+  }
+
+  @Post('few-shot-prompting')
+  @ApiBody({ type: FewShotPrompt })
+  async fewShotPrompting(@Body() prompts: FewShotPrompt): Promise<string> {
+    return await this.openAiService.callOpenAiWithFewShotPrompts(prompts);
+  }
+}
+
+interface RolePrompt {
+  role: 'system' | 'user' | 'assistant';
+  content: string;
 }
--- src/open-ai/open-ai.service.ts
+++ src/open-ai/open-ai.service.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import * as process from 'process';
 import { Injectable } from '@nestjs/common';
+import { FewShotPrompt } from './open-ai.controller';
+import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from 'openai/src/resources/chat/completions';
 
 const apiKey = process.env.OPEN_AI_API_KEI;
+const openai = new OpenAI({ apiKey: apiKey });
 
 @Injectable()
 export class OpenAiService {
@@ -59,4 +63,32 @@ export class OpenAiService {
       console.error(error);
     }
   }
+
+  async callOpenAiWithFewShotPrompts(prompts: FewShotPrompt) {
+    console.log(prompts);
+
+    try {
+      const messages: Array<ChatCompletionMessageParam> = prompts.prompts.map(
+        (p) => ({
+          role: p.role,
+          content: p.content,
+        }),
+      );
+      const chatCompletion = await openai.chat.completions.create({
+        messages: messages,
+        model: 'gpt-3.5-turbo-16k-0613',
+        // max_tokens: 100,
+      });
+
+      return chatCompletion.choices[0].message.content;
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
+
+// EG prompts
+// { role: 'system', content: 'You are a helpful assistant.' },
+// { role: 'user', content: 'Who won the world series in 2020?' },
+// { role: 'assistant', content: 'The Los Angeles Dodgers won the World Series in 2020.' },
+// { role: 'user', content: 'Where was it played?' }
